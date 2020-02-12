e66b36894c283816eed58a18cffb7f880f4bbe96
Kristian <krdo@eaaa.dk>
2020-02-12 08:29:42 +0100

    Exercise 1 examples.

--- lesson02/react-examples/src/App.js
+++ lesson02/react-examples/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ButtonExample from "./ButtonExample";
 import InputTextExample from "./InputTextExample";
 import HelloExample from "./HelloExample";
+import NameListExample from "./NameListExample";
 
 class App extends Component {
 
@@ -9,9 +10,14 @@ class App extends Component {
         return (
             <>
               <HelloExample just_a_name="fellow programmer"/> {/* just_a_name is a prop */}
+
               <ButtonExample/>
+
               <br/><br/>
+
               <InputTextExample/>
+
+              <NameListExample names={['Kristian', 'Jes', 'Mads', 'Torill']}/>
             </>
         );
     }

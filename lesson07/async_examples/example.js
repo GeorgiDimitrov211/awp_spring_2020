2e383520beeb5f2abff0dc6080dda6cedd29b04b
kdorland <kristian@dorland.dk>
2020-03-24 09:31:42 +0100

    Removing unfinished example.

--- lesson07/async_examples/example.js
+++
@@ -1,9 +0,0 @@
-async function sumAsync(a, b) {
-    return (a + b);
-}
-
-sumAsync(2,2).then(result => console.log(result));
-const result = async _ => await sumAsync(2,2);
-console.log(result);
-
-// Original style
